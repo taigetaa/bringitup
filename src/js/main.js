eeca39be47bdda669f027cdd4d4346c99725b2fc
Victoria <bethmorgendorffer@mail.ru>
2023-08-06 23:28:48 +0300

    videoplayer

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,9 @@
 import Slider from "./modules/slider";
+import videoPlayer from "./modules/playVideo";
 
 window.addEventListener("DOMContentLoaded", () => {
     const slider = new Slider(".page", ".next");
     slider.render();
+    const player = new videoPlayer(".showup .play", ".overlay");
+    player.init();
 });
