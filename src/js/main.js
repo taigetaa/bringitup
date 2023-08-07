038204ebb0595bff72aae0a9d52120e60f87161a
Victoria <bethmorgendorffer@mail.ru>
2023-08-07 16:37:45 +0300

    sliders

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,11 @@
-import Slider from "./modules/slider";
 import videoPlayer from "./modules/playVideo";
+import mainSlider from "./modules/slider/slider-main";
 
 window.addEventListener("DOMContentLoaded", () => {
-    const slider = new Slider(".page", ".next");
+    const slider = new mainSlider({btns: '.next', page: '.page'});
     slider.render();
+
     const player = new videoPlayer(".showup .play", ".overlay");
     player.init();
+
 });
