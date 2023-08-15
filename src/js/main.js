45b1dbace00d416a9f020f9a48b982f470e43a9b
Victoria <bethmorgendorffer@mail.ru>
2023-08-15 18:42:21 +0300

    second page sliders

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,9 @@ window.addEventListener("DOMContentLoaded", () => {
     const slider = new mainSlider({btns: '.next', container: '.page'});
     slider.render();
 
+    const modulePageSlider = new mainSlider({container: ".moduleapp", btns: '.next'});
+    modulePageSlider.render();
+
     const showUpSlider = new MiniSlider({
         container: '.showup__content-slider',
         next: '.showup__next',
